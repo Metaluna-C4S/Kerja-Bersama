296a01c7074d0bc13e49fc688fa7976b4f6b2f66
Metaluna-C4S <44105327+Metaluna-C4S@users.noreply.github.com>
2018-10-27 14:38:54 +0700

    Add training template

---
+++ Project.Prospect/assets/styles/main.js
@@ -0,0 +1,13 @@
+var testimonySlider = document.getElementsByClassName("testimony-quotes")[0];
+
+function test1func() {
+    testimonySlider.style.left = 0;
+}
+
+function test2func() {
+    testimonySlider.style.left = -510 + 'px';
+}
+
+function test3func() {
+    testimonySlider.style.left = -1020 + 'px';
+}
